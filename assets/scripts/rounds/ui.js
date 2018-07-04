fc945b610413197da2a59e229206846e261204e6
michaelmolchan <mmolchan91@gmail.com>
2018-07-04 14:09:07 -0400

    Update success/error messages for Add Round form
    - Add fade in error alert to add round form
    - Add success modal to add round form

--- assets/scripts/rounds/ui.js
+++ assets/scripts/rounds/ui.js
@@ -5,10 +5,13 @@
 const addRoundSuccess = function (addRoundResponse) {
   console.log(addRoundResponse)
   $('#add-round-form')[0].reset()
+  $('#successModal').modal('show')
+  $('#success-message').html('Your round has been added to your profile!')
 }
 
 const addRoundError = function (addRoundError) {
   $('#add-round-form')[0].reset()
+  $('#add-round-error').delay(100).fadeIn(100)
 }
 
 module.exports = {
